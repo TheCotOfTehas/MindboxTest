11bb0b8a49e4d4842860da2620a8eabaed9d04c5
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-07-29 20:19:42 +0300

    Создание графа и Триноды

---
+++ Graph/Graph/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class Graph
+    {
+        public readonly List<TreeNode> Nodes = new List<TreeNode>();
+
+
+    }
+}
---
+++ Graph/Graph/TreeNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class TreeNode
+    {
+        char Name { get; set; }
+        public readonly List<TreeNode> Children = new List<TreeNode>();
+
+        public TreeNode(char name)
+        {
+            Name = name;
+        }
+
+        public IEnumerable<TreeNode> IncidentNodes
+        {
+            get
+            {
+                foreach (var node in Children)
+                    yield return node;
+            }
+        }
+
+        public void Connect(TreeNode node)
+        {
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+
+            if (Children.Count >= 3)
+                throw new ArgumentOutOfRangeException(nameof(node), "Тринода не может содержать более трёх детей");
+
+            Children.Add(node);
+        }
+
+        IEnumerable<TreeNode> GetAllNodes(TreeNode node)
+        {
+
+        }
+    }
+}
